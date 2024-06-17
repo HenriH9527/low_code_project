355082b2dc20c0ce87d5927cb878aab0c12b2c92
HenriH9527 <1004779785@qq.com>
2024-06-17 15:54:24 +0800

    feat: 配置dock erdocker

--- low-code/apps/low-code-test/src/app.controller.ts
+++ low-code/apps/low-code-test/src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Version, VERSION_NEUTRAL } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller({
+  path: 'user',
+  version: '1',
+})
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get()
+  @Version([VERSION_NEUTRAL, '1'])
+  findAll() {
+    return 'i am old one';
+  }
+
+  @Get()
+  @Version('2')
+  findAll2() {
+    return 'i am new one';
   }
 }
