6924a3fbb3b988c71394cedea593a0c04354221e
Turbobot <turbobot@vercel.com>
2024-06-17 11:19:09 +0800

    feat(create-turbo): apply official-starter transform

--- low-code/apps/devops/src/main.ts
+++ low-code/apps/devops/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DevopsModule } from './devops.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(DevopsModule);
   await app.listen(3000);
 }
 bootstrap();
